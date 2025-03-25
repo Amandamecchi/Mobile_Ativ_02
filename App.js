230806c1ae1bf579d9183bd9aeb092401d12009e
Amanda Gomes Mechi <amandamechi327@gmail.com>
2025-03-25 13:59:59 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>OBRIGADA A TODOS QUE CONFIARAM EM MIM❤️ </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'pink',
+    textAlign: 'center',
+    margin: 40,
   },
 });
